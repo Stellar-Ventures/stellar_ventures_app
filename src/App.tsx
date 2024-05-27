f829510ffb04ae8fbc9077116fc261a83bad09a1
AlanD <avlasenko@arbat.dev>
2024-05-27 23:55:57 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import './App.css';
 import { TonConnectButton } from '@tonconnect/ui-react';
 import '@twa-dev/sdk'
-import WebApp from '@twa-dev/sdk'
 import { Settings } from './pend'
-import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <div>
       <TonConnectButton />
       <Settings />
     </div>
-    
   );
 }
 
 export default App
-
-
