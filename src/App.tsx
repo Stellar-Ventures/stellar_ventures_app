bac5699b1b785023065d3bfad1e6a0266da5cd64
AlanD <avlasenko@arbat.dev>
2024-05-27 20:14:47 +0300

    add  payments

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { TonConnectButton } from '@tonconnect/ui-react';
 import'@twa-dev/sdk'
+import { Settings } from './pend';
 
 function App() {
   return (
     <div>
       <TonConnectButton />
+      <Settings />
     </div>
-    
   );
 }
 
--- src/pend.tsx
+++ src/pend.tsx
@@ -1,23 +1,23 @@
-// import { useTonConnectUI } from '@tonconnect/ui-react';
+import { useTonConnectUI } from '@tonconnect/ui-react';
 
-// const transaction = {
-//     messages: [
-//         {
-//             address: "0:412410771DA82CBA306A55FA9E0D43C9D245E38133CB58F1457DFB8D5CD8892F", // destination address
-//             amount: "20000000" //Toncoin in nanotons
-//         }
-//     ]
+const transaction = {
+    messages: [
+        {
+            address: "0QCAttGbzwoLkW5Db3paKixtx1ydHqtsd-hp-NupRwOn54SB",
+            amount: "20000000"
+        }
+    ],
+    validUntil: Math.floor(Date.now() / 1000) + 3600
+};
 
-// }
-
-// export const Settings = () => {
-//     const [tonConnectUI, setOptions] = useTonConnectUI();
-
-//     return (
-//         <div>
-//             <button onClick={() => tonConnectUI.sendTransaction(transaction)}>
-//                 Send transaction
-//             </button>
-//         </div>
-//     );
-// };
+export const Settings = () => {
+    const [tonConnectUI] = useTonConnectUI();
+    console.log(transaction)
+    return (
+        <div>
+            <button onClick={() => tonConnectUI.sendTransaction(transaction)}>
+                Send transaction
+            </button>
+        </div>
+    );
+};
