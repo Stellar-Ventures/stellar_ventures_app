0136a1417c9d10df9a540ecfbcc1a28b663067ad
AlanD <avlasenko@arbat.dev>
2024-05-27 23:46:01 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import './App.css';
 import { TonConnectButton } from '@tonconnect/ui-react';
-import'@twa-dev/sdk'
-import { Settings } from './pend';
+import '@twa-dev/sdk'
+
+import { Settings } from './pend'
+import { useState } from 'react'
+import WebApp from '@twa-dev/sdk'
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div>
       <TonConnectButton />
       <Settings />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="card">
+        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+            Show Alert
+        </button>
+      </div>
     </div>
+    
   );
 }
 
