adc73f82fb427af8c4c40813548e6a0e00a8e1bd
AlanD <avlasenko@arbat.dev>
2024-05-27 16:15:12 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import './App.css';
 import { TonConnectButton } from '@tonconnect/ui-react';
+import { useTonAddress } from '@tonconnect/ui-react';
 import'@twa-dev/sdk'
 
 function App() {
   return (
+    
     <div>
       <TonConnectButton />
     </div>
+    
   );
 }
 
 export default App
+
+export const Address = () => {
+  const userFriendlyAddress = useTonAddress();
+  const rawAddress = useTonAddress(false);
+
+  return (
+      userFriendlyAddress && (
+          <div>
+              <span>User-friendly address: {userFriendlyAddress}</span>
+              <span>Raw address: {rawAddress}</span>
+          </div>
+      )
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { Address } from './App';
 
 // this manifest is used temporarily for development purposes
 const manifestUrl = './public/tonconecnt-manifest.json';
@@ -9,5 +10,6 @@ const manifestUrl = './public/tonconecnt-manifest.json';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl} >
     <App />
+    <Address />
   </TonConnectUIProvider>,
 )
