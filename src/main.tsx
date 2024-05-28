145eb2b245926d129dcb4f4494cc950b00579f09
AlanD <avlasenko@arbat.dev>
2024-05-28 15:30:13 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <TonConnectUIProvider manifestUrl={VITE_MANIFEST_URL} >
+  <TonConnectUIProvider manifestUrl={import.meta.env.VITE_MANIFEST_URL} >
     <App />
   </TonConnectUIProvider>,
 )
